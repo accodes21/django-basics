4f9cbecba97b122cce05aee91d845d75567e626d
Aarya Chopkar <aaryachopkar@gmail.com>
2024-03-29 22:02:29 +0530

    updated index.html

--- home/views.py
+++ home/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+
+    text = """Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam doloremque, inventore soluta perferendis dolorum minus vel ad ab, dolore quaerat, rerum quidem. Nobis nam perspiciatis ipsa quasi libero consequatur asperiores."""
+
+    vegetables = ["tomato", "onion", "cabbage"]
+
+    return render(request, 'home/index.html', context={'text': text, 'vegetables':vegetables})
