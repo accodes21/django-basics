ca8799375e1ac4bd114e09cc5d65e3f4e5569a44
Aarya Chopkar <aaryachopkar@gmail.com>
2024-04-01 21:06:03 +0530

    added CRUD ops in recipe app

--- core/urls.py
+++ core/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('about/', about, name="about"),
     path('recipe/', recipe, name="recipe"),
     path('delete-recipe/<id>/', delete_recipe, name='delete_recipe'),
+    path('update-recipe/<id>/', update_recipe, name='update_recipe'),
     path('admin/', admin.site.urls),
 ]
--- food/views.py
+++ food/views.py
@@ -19,9 +19,36 @@ def recipe(request):
         return redirect('/recipe/')
     
     queryset = Receipe.objects.all()
+
+    if request.GET.get('search'):
+        queryset = queryset.filter(receipe_name__icontains = request.GET.get('search'))
+
     context = {'recipes': queryset}
 
     return render(request, 'recipe.html', context)
+
+def update_recipe(request, id):
+    queryset = Receipe.objects.get(id = id)
+
+    if request.method == "POST":
+        data = request.POST
+
+        recipe_name = data.get("recipe_name")
+        recipe_desc = data.get("recipe_desc")
+        recipe_image = request.FILES.get("recipe_image")
+
+        queryset.receipe_name = recipe_name
+        queryset.receipe_description = recipe_desc
+
+        if recipe_image:
+            queryset.receipe_image = recipe_image
+        
+        queryset.save()
+        return redirect('/recipe/')
+
+    context = {'recipe': queryset}
+    return render(request, 'update_recipe.html', context)
+
 
 def delete_recipe(request, id):
     queryset = Receipe.objects.get(id = id)
