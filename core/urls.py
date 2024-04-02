a62feb6a9b5f2670b12a7e423ea1d2beea757e18
Aarya Chopkar <aaryachopkar@gmail.com>
2024-04-02 18:42:59 +0530

    media files displayed

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from home.views import *
 from food.views import *
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', home, name="home"),
@@ -28,3 +31,10 @@ urlpatterns = [
     path('update-recipe/<id>/', update_recipe, name='update_recipe'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
